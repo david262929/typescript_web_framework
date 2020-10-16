10055f867786220f439865c7410cbfa5c12576fc
David <davidghazaryan2000@gmail.com>
2020-10-16 17:41:58 +0400

    User Model using

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hi')
+import {UserProps} from './Models/User/types'
+import {User} from './Models/User'
+
+const userProps: UserProps = { 
+    name: 'Test_name', 
+    age: 20,
+}
+const user = new User(userProps);
