84c6927f1df72756c1f68704c51168d8a89f25d2
David <davidghazaryan2000@gmail.com>
2020-10-19 13:11:33 +0400

    checkouted to userEdit View

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,5 @@
-// import { User } from './Models/User';
-// import {UserEdit} from './Views/UserEdit';
-
-// const root: HTMLElement | null = document.getElementById('root');
-
-// if(!root){
-// 	throw new Error(`Can't find a root element`);
-// }
-
-// const userEdit = new UserEdit(
-// 	root,
-// 	User.buildUser({
-// 		name: 'Davo',
-// 		age: 20,
-// 	})
-// )
-
-// userEdit.render();
-
-
-import { UserList } from './Views/UserList'
-import { UserProps } from './Models/User/types'
-import { User } from './Models/User'
+import { User } from './Models/User';
+import {UserEdit} from './Views/UserEdit';
 
 const root: HTMLElement | null = document.getElementById('root');
 
@@ -28,8 +7,29 @@ if(!root){
 	throw new Error(`Can't find a root element`);
 }
 
-(async (): Promise<void> => {
-	const collection = User.buildUserCollection()
-	await collection.fetch();
-	new UserList(root, collection).render();
-})()
+const userEdit = new UserEdit(
+	root,
+	User.buildUser({
+		name: 'Davo',
+		age: 20,
+	})
+)
+
+userEdit.render();
+
+
+// import { UserList } from './Views/UserList'
+// import { UserProps } from './Models/User/types'
+// import { User } from './Models/User'
+
+// const root: HTMLElement | null = document.getElementById('root');
+
+// if(!root){
+// 	throw new Error(`Can't find a root element`);
+// }
+
+// (async (): Promise<void> => {
+// 	const collection = User.buildUserCollection()
+// 	await collection.fetch();
+// 	new UserList(root, collection).render();
+// })()
